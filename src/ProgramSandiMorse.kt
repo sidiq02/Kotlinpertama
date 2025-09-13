2a1710fac04a8738be7b33e53a1ac293db5be77d
sidiq02 <emailsidiq02@gmail.com>
2025-09-13 13:52:41 +0700

    commit Progmsandimorse dan Perintah Masukkan

--- src/ProgramSandiMorse.kt
+++ src/ProgramSandiMorse.kt
@@ -4,41 +4,21 @@ fun main(args: Array<String>) {
     // the string which we want to decode
     val s = ".. -.-. - ... --- -.-. .. .- .-.."
     println("The original message: $s")
-
     // the string with the decoded message
     var message = ""
-
     // array definitions
     val alphabetChars = "abcdefghijklmnopqrstuvwxyz"
     val morseChars = arrayOf(".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-",
         ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "-..")
-
-
-
-
     // splitting the string into Morse characters
-
-
     val
-            characters
-            =
-        s.split("")
-
-
-
-
+            characters = s.split("")
                     //iterating over Morse characters
             for
                     (morseChar
             in
             characters)
             {
-
-
-
-
-
-
                 val
                         index
                         =
